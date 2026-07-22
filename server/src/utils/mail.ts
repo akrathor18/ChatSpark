42579d770534bb870597bb78696f04e38d470065
Ashish <127130546+akrathor18@users.noreply.github.com>
2026-07-22 13:35:15 +0530

    fix: update email transporter configuration for improved compatibility

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -1,7 +1,12 @@
+import dns from "dns";
 import nodemailer from "nodemailer";
 
+dns.setDefaultResultOrder("ipv4first");
+
 export const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
